98e8ee3c8f6088fc778e90220faa0026ffd1e881
Facundo Amarilla <facundogabriel91@gmail.com>
2023-06-10 13:53:12 -0300

    Acceso Datos terminado

--- Data/DataAccess.cs
+++ Data/DataAccess.cs
@@ -12,14 +12,16 @@ namespace Data
         SqlDataReader reader = null;
         SqlCommand cmd = null;
         SqlConnection connection;
+        public SqlDataReader Lector { get { return reader; } }
 
         //TODO: Abrir Conexion
         public void AbrirConexion(string path, string server = ".")
         {
-            path = $"server={server}; database = CATALOGO_P3_DB; integrated security = true";
+            // cambiar 'database' al nombre de la bd a crear
+            path = $"server={server}; database = CATALOGO_TPFinal; integrated security = true";
             try
             {
-                connection = new SqlConnection(server);
+                connection = new SqlConnection(path);
                 connection.Open();
             }
             catch (SqlException ex)
@@ -31,6 +33,7 @@ namespace Data
                 throw ex;
             }
         }
+
         //TODO: Cerrar Conexion
         public void CerrarConexion()
         {
@@ -50,6 +53,71 @@ namespace Data
             catch (SqlException ex)
             {
                 throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //TODO: Setear Query
+        public void SetQuery(string query, string tipo)
+        {
+            // si tipo == 'query', es una consulta normal, si tipo == 'sp', es una consuilta mediante stored procedure
+            try
+            {
+                cmd = new SqlCommand();
+
+                if(tipo == "query")
+                    cmd.CommandType = System.Data.CommandType.Text;
+                else if(tipo == "sp")
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.CommandText = query;
+                cmd.Connection = connection;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //TODO: Ejecutar Query
+        public int ExecuteQuery()
+        {
+            try
+            {
+               return cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex) 
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //TODO: Leer Datos
+        public void ReadQuery()
+        {
+            try
+            {
+                reader = cmd.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //TODO: Setear Parametros
+        public void SetParameters(string name, object value)
+        {
+            try
+            {
+                cmd.Parameters.AddWithValue(name, value);
             }
             catch (Exception ex)
             {
