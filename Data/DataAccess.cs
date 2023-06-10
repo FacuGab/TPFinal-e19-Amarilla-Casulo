931ef2695b66e61f8a31e004db9405b3424b6de7
Facundo Amarilla <facundogabriel91@gmail.com>
2023-06-10 13:28:48 -0300

    Conexion BD

---
+++ Data/DataAccess.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Data
+{
+    public class DataAccess
+    {
+        SqlDataReader reader = null;
+        SqlCommand cmd = null;
+        SqlConnection connection;
+
+        //TODO: Abrir Conexion
+        public void AbrirConexion(string path, string server = ".")
+        {
+            path = $"server={server}; database = CATALOGO_P3_DB; integrated security = true";
+            try
+            {
+                connection = new SqlConnection(server);
+                connection.Open();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        //TODO: Cerrar Conexion
+        public void CerrarConexion()
+        {
+            try
+            {
+                if(reader != null) 
+                {
+                    reader.Close();
+                    reader.Dispose();
+                }
+                if(cmd != null)
+                    cmd.Dispose();
+
+                connection.Close();
+                connection.Dispose();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
